0f4ea0a71f27ac3d5240520b65ee8fc7a921438f
Stephan Zhu <68916795+zhuzhuu@users.noreply.github.com>
2024-12-05 17:49:15 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ window._config = {
         userPoolId: 'us-east-1_NTbYajK0t', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5v76iign6i2gv04tphov6uie2r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
+        secret_hash: '1kruv1b85e7du5vjkg8dfpc10a9tb358ssf6mo6gej781fi0uuj7'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
