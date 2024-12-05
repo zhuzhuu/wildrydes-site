a18b636966bd6c27c8c108517481dc542acbdfa9
Stephan Zhu <68916795+zhuzhuu@users.noreply.github.com>
2024-12-05 17:37:53 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_NTbYajK0t', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5v76iign6i2gv04tphov6uie2r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
